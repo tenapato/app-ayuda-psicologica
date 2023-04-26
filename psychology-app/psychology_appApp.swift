5386a04a073426aa642e3888d6de6aa22eaa2355
tenapato <tenapatricio@gmail.com>
2023-04-25 21:09:46 -0600

    Initial Commit

---
+++ psychology-app/psychology_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  psychology_appApp.swift
+//  psychology-app
+//
+//  Created by Patricio Tena on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct psychology_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
